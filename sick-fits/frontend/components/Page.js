508f839072ac1fb55b94a2b671b9273ea3e5ff72
Billy Bunn <williammbunn@gmail.com>
2019-07-07 16:27:33 -0700

    adds global styles, visualizes route changes, and fixes FOUC

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,66 @@
 import React from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Meta from './Meta';
 import Header from './Header';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StyledPage = styled.div`
+  background: #fff;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+@font-face {
+  font-family: 'radnika_next';
+  font-style: normal;
+  font-weight: normal;
+  src: url('/static/radnikanext-medium-webfont.woff2');
+}
+html {
+  box-sizing: border-box;
+  font-size: 10px;
+}
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'radnika_next';
+  font-size: 1.5rem;
+  line-height: 2;
+  margin: 0;
+  padding: 0;
+}
+a {
+  color: ${theme.black};
+  text-decoration: none;
+}
+`;
+
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
