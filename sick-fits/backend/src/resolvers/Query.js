fd2960a866bfc0f61036a9902ae3b5a2ada06ae5
Billy Bunn <williammbunn@gmail.com>
2019-07-15 14:20:36 -0700

    adds middleware and render props for logged in user

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,12 +3,19 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
-
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null; // don't throw error in case not logged in
+    }
+    return ctx.db.query.user( // returns a promise
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
